724d95b384a68f2aecaddd79564c8ed27107fa92
Drake Axelrod <drake@draxel.io>
2021-12-29 22:32:49 +0100

    probably broke something

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,7 +1,7 @@
 import { Req, Res } from "types/common";
 
 const handler = async (req: Req, res: Res) => {
-  res.status(200).json(JSON.stringify({}));
+  res.status(200).json({});
 };
 
 export default handler;
